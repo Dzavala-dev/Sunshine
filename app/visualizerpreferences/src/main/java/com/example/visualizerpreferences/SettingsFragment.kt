a51805e9bc4bd1e50393c38eb828b6a44fba92a6
Dzavala-dev <david.zavala@mercadolibre.com.mx>
2021-02-09 14:51:19 -0600

    Agregar exceptions para el size scale
    Edit Text Preference Constriants

--- app/visualizerpreferences/src/main/java/com/example/visualizerpreferences/SettingsFragment.kt
+++ app/visualizerpreferences/src/main/java/com/example/visualizerpreferences/SettingsFragment.kt
@@ -4,11 +4,13 @@ import android.content.SharedPreferences
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.example.com.visualizerpreferences.R
 import android.os.Bundle
+import android.widget.Toast
 import androidx.preference.*
 
 
+// COMPLETED (1) Implement OnPreferenceChangeListener
 class SettingsFragment : PreferenceFragmentCompat(),
-    OnSharedPreferenceChangeListener {
+    OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
     override fun onCreatePreferences(bundle: Bundle?, s: String?) {
 
         // Add visualizer preferences, defined in the XML file in res->xml->pref_visualizer
@@ -24,9 +26,14 @@ class SettingsFragment : PreferenceFragmentCompat(),
             // they are already set up in xml using summaryOff and summary On
             if (p !is CheckBoxPreference) {
                 val value = sharedPreferences.getString(p.key, "")
-                p.setPreferenceSummary(value)
+                setPreferenceSummary(p, value)
             }
         }
+
+        // COMPLETED (3) Add the OnPreferenceChangeListener specifically to the EditTextPreference
+        // Add the preference listener which checks that the size is correct to the size preference
+        val preference: Preference = findPreference(getString(R.string.pref_size_key))
+        preference.onPreferenceChangeListener = this
     }
 
     override fun onSharedPreferenceChanged(
@@ -37,30 +44,62 @@ class SettingsFragment : PreferenceFragmentCompat(),
         val preference: Preference = findPreference(key)
         if (preference !is CheckBoxPreference) {
             val value = sharedPreferences.getString(preference.key, "")
-            preference.setPreferenceSummary(value)
+            setPreferenceSummary(preference, value)
         }
     }
 
     /**
      * Updates the summary for the preference
      *
-     * @param this@setPreferenceSummary The preference to be updated
+     * @param preference The preference to be updated
      * @param value      The value that the preference was updated to
      */
-    private fun Preference.setPreferenceSummary(value: String?) {
-        // COMPLETED (3) Don't forget to add code here to properly set the summary for an EditTextPreference
-        if (this is ListPreference) {
+    private fun setPreferenceSummary(preference: Preference, value: String?) {
+        if (preference is ListPreference) {
             // For list preferences, figure out the label of the selected value
-            val listPreference: ListPreference = this
+            val listPreference: ListPreference = preference as ListPreference
             val prefIndex: Int = listPreference.findIndexOfValue(value)
             if (prefIndex >= 0) {
                 // Set the summary to that label
                 listPreference.summary = listPreference.entries[prefIndex]
             }
-        } else if (this is EditTextPreference) {
+        } else if (preference is EditTextPreference) {
             // For EditTextPreferences, set the summary to the value's simple string representation.
-            setSummary(value)
+            preference.setSummary(value)
         }
+    }
+
+    // COMPLETED (2) Override onPreferenceChange. This method should try to convert the new preference value
+    // to a float; if it cannot, show a helpful error message and return false. If it can be converted
+    // to a float check that that float is between 0 (exclusive) and 3 (inclusive). If it isn't, show
+    // an error message and return false. If it is a valid number, return true.
+    override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
+        // In this context, we're using the onPreferenceChange listener for checking whether the
+        // size setting was set to a valid value.
+        val error = Toast.makeText(
+            context,
+            "Please select a number between 0.1 and 3",
+            Toast.LENGTH_SHORT
+        )
+
+        // Double check that the preference is the size preference
+        val sizeKey: String = getString(R.string.pref_size_key)
+        if (preference.key == sizeKey) {
+            val stringSize = newValue as String
+            try {
+                val size = stringSize.toFloat()
+                // If the number is outside of the acceptable range, show an error.
+                if (size > 3 || size <= 0) {
+                    error.show()
+                    return false
+                }
+            } catch (nfe: NumberFormatException) {
+                // If whatever the user entered can't be parsed to a number, show an error
+                error.show()
+                return false
+            }
+        }
+        return true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
